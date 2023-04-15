fcaccd19555dedb0bef69148872f47500fe60b65
Faizun Musthofa <ineffablely.official@gmail.com>
2023-04-15 13:12:13 +0700

    v0.1.2alpha

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Anime extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["title", "slug", "episodes", "status", "type", "aired_from", "aired_to", "season", "studio", "genres", "descriptions", "images", "images_square"];
 }
